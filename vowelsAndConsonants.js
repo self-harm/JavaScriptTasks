fba63c8d29e668c60e2744a71b483efaf000e97e
self-harm <lightgraywrite@gmail.com>
2021-02-04 13:31:52 +0300

    Update vowelsAndConsonants.js

--- vowelsAndConsonants.js
+++ vowelsAndConsonants.js
@@ -2,6 +2,15 @@
  * Complete the vowelsAndConsonants function.
  * Print your output using 'console.log()'.*/
 
+/*Task
+
+Complete the vowelsAndConsonants function in the editor below. It has one parameter, a string, ,
+consisting of lowercase English alphabetic letters (i.e., a through z). The function must do the following:
+
+First, print each vowel in  on a new line. The English vowels are a, e, i, o, and u, and each vowel
+must be printed in the same order as it appeared in .
+Second, print each consonant (i.e., non-vowel) in  on a new line in the same order as it appeared in .*/
+
 function vowelsAndConsonants(s) {
     //I believe there is a better way to illustrate a regex
     const vowelRegex = s.match(/[aeiou]/ig);
