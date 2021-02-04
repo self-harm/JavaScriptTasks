9547e4690a011497926385948452ae7b9e6b4a84
self-harm <lightgraywrite@gmail.com>
2021-02-04 13:32:01 +0300

    Update vowelsAndConsonants.js

--- vowelsAndConsonants.js
+++ vowelsAndConsonants.js
@@ -9,6 +9,7 @@ consisting of lowercase English alphabetic letters (i.e., a through z). The func
 
 First, print each vowel in  on a new line. The English vowels are a, e, i, o, and u, and each vowel
 must be printed in the same order as it appeared in .
+
 Second, print each consonant (i.e., non-vowel) in  on a new line in the same order as it appeared in .*/
 
 function vowelsAndConsonants(s) {
